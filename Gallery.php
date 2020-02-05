f54c9bb482c230c9efb3cb338ae75eeab35ba024
R1CHYR1CH <45674924+R1CHYR1CH@users.noreply.github.com>
2020-02-05 13:04:55 +1000

    Added filter by type + Grouped images by Dir

--- Gallery.php
+++ Gallery.php
@@ -2,10 +2,14 @@
 class Gallery {
 
     private $directory;
-    private $files = array();
-
-    public function __construct($directory) {
+    private $files = [];
+    private $temp = [];
+    
+    const _validFiles = ['jpeg','jpg','png'];
+    
+    public function __construct($directory, $fileType) {
         $this->directory = $directory;
+        $this->fileType = $fileType;
     }
 
     public function load() {
@@ -15,30 +19,53 @@ class Gallery {
         $it = new RecursiveDirectoryIterator($this->directory);
         // for each file found
         foreach(new RecursiveIteratorIterator($it) as $item){    
-            // keep only images
-            if (@in_array(strtolower(array_pop(explode('.', $item))), Array ('jpeg','jpg','png'))){ 
-                $imageDetails   = getimagesize($item);
-                $imageSize      = $imageDetails[3];
-                $imageType      = $imageDetails['mime'];
-                $this->files[]  = array($item,$imageSize,$imageType);
+            // filter by image type
+            $filter = (!$this->fileType || $this->fileType == 'all' ) ? self::_validFiles : [$this->fileType];
+            // read the file extention and check if it's allowed. 
+            $parts = explode('.', $item);
+            $fileExtention = end($parts);
+            if (in_array($fileExtention, $filter)){
+                // extention is ok 
+                // get image details
+                $imageDetails = getimagesize($item);
+                $imageSize = $imageDetails[3];
+                //$imageType = $imageDetails['mime']; // Use the file type from mime data
+                $imageType = $fileExtention; // Use the file type from extention
+                // get image dir location as required
+                $info = new SplFileInfo($item);
+                $parent_info = $info->getPathInfo();
+                $imageDir = $parent_info->getRealPath();
+                // add the required details to the files array.
+                $this->temp[] = [$item, $imageSize, $imageType, $imageDir];
             }
+            // group array by $imageDir - this make the $this->files array
+            foreach ($this->temp as $key => $item) {
+               $this->files[$item[3]][$key] = $item;
+            }
+            // alphabetically sort for no particular reason!
+            ksort($this->files, SORT_STRING );
         }
         $directory->close();
     }
-
+    
     public function render() {
-        // create basic (Very basic output for images)
-        echo'<table class="table">';
-        echo'<th></th><th>Size</th><th>Image type</th> ';
-        // crate a id for each image to keep lightbox happy
-        $imageId = 'id'.uniqid();
-        // out put a table row for each image
+        // for each dir crate a new table
         foreach ($this->files as $file) {
-            echo "<tr>"; 
-                echo '<td><a href="' . htmlspecialchars($file[0]) . '" data-lightbox="' . $imageId . '" title="' . htmlspecialchars($file[1]) . '"><img src="' . htmlspecialchars($file[0]) . '" alt="" style="width:200px;"></a></td>';
-                echo '<td>'. $file[1]. '</td>';
-                echo '<td>'. $file[2].'</td>';
-            echo '</tr>'; 
+            // use dir as title
+            echo '<table class="table">';
+            echo '<th></th><th>Dir</th><th>Size</th><th>Type</th> ';            
+                // for each file
+                foreach ($file as $f) {
+                // crate a id for each image to keep lightbox happy
+                $imageId = 'id' . uniqid();
+                // out put a table row for each image
+                echo "<tr>"; 
+                    echo '<td><a href="' . htmlspecialchars($f[0]) . '" data-lightbox="' . $imageId . '" title="' . htmlspecialchars($f[1]) . '"><img src="' . htmlspecialchars($f[0]) . '" alt="" style="width:150px;"></a></td>';
+                    echo '<td>' . htmlspecialchars($f[3]) . '</td>';
+                    echo '<td>' . htmlspecialchars($f[1]) . '</td>';
+                    echo '<td>' . htmlspecialchars($f[2]) . '</td>';
+                echo '</tr>'; 
+            }
         }
         echo'</table>';
     }
--- index.php
+++ index.php
@@ -1,35 +1,71 @@
+<?php
+// clean GET arrays
+$R = filter_input_array(INPUT_GET);
+// include the Gallery class
+require_once 'Gallery.php';
+// get requested file type
+// check if file type it is allowed - listed in an array here Gallery::_validFiles
+// If no file type requested default to "all"
+$requestFileType = (in_array($R['type'], Gallery::_validFiles)) ? $R['type'] : 'all';
+// create new Gallery object
+$gallery = new Gallery('./images', $requestFileType);
+// Loads images to gallary object
+$gallery->load();
+?>
 <!doctype html>
 <html lang="en">
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <!--lightbox2-->
+        <!--lightbox css -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.min.css">
-        
-        <title>DS PART2</title>
+        <title>Image Gallery</title>
+        <style>
+            .table {
+                background-color: #eee; 
+                margin: 50px 0;
+            }
+            .table th {
+                padding: .75rem;
+                background-color: #ddd; 
+                border-top: 1px solid #555;
+                border-bottom: 1px solid #555;
+            }       
+        </style>
     </head>
     <body>
-
-        <h1>Part 2</h1>
-
-        <?php
-        require_once('Gallery.php');
-        $gallery = new Gallery('./images');
-        $gallery->load(); // Loads images
-        $gallery->render(); // Prints HTML table
-        ?>
+        <div class="container">
+            <h1>Image Gallery</h1>  
+            <!-- filter by file type -->
+            <form action="index.php" method="get">
+                <select name="type">
+                    <option>all</option>
+                    <?php
+                        // Display all file types that are allowed
+                        // Make sure the current file type is active
+                        foreach (Gallery::_validFiles as $t) {
+                            $active = ($R['type'] == $t) ? 'selected' : '';
+                            echo "<option $active>$t</option>";
+                        }
+                    ?>
+                </select>
+                <input type="submit" />
+            </form>    
+            <?php
+            // Render class HTML output
+            $gallery->render();
+            ?>
+        </div>
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        
+        <!-- lightbox JS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox-plus-jquery.js"></script>
-        
     </body>
 </html>
